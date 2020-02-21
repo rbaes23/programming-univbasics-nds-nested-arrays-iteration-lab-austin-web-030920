5edeca89f70207ed113208492c707a2248f9149b
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-21 18:25:18 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,15 +9,15 @@ def join_ingredients(src)
   
   new_array = []
   row_index = 0
-    while row_index < array.count do
+    while row_index < src.count do
       element_index = 0
-        while element_index < array[row_index].count do
-          puts array[row_index][element_index]
+        while element_index < src[row_index].count do
+          puts src[row_index][element_index]
           element_index += 1
         end
         row_index += 1
      end
-  return array
+  return src
 end
 
 
