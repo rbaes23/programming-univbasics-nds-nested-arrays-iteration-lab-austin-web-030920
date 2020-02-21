6b0f5ef877eb015672d8b1d80062ddf019dd6ab6
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-21 19:24:38 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,12 +6,47 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  new_array = []
+  row_index = 0
+    while row_index < src.count do
+      element_index = 0
+        while element_index < src[row_index].count do
+          first_ingredient = src[row_index][element_index]
+          element_index += 1
+          second_ingredient = src[row_index][element_index]
+          new_array << "I love #{first_ingredient} and #{second_ingredient} on my pizza"
+          element_index += 1
+        end
+        row_index += 1
+     end
+  return new_array
 end
+
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  outer_results = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    inner_results = []
+      while element_index < src[row_index].count do
+        first_num = src[row_index][element_index]
+        element_index += 1
+        second_num = src[row_index][element_index]
+        if first_num > second_num
+          inner_results << first_num
+        else
+          inner_results << second_num
+        end
+        element_index += 1
+      end
+       outer_results << inner_results[0]
+       row_index += 1
+  end
+  return outer_results
 end
 
 def total_even_pairs(src)
@@ -22,4 +57,20 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  total = 0
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+      while element_index < src[row_index].count do
+        first_num = src[row_index][element_index]
+        element_index += 1
+        second_num = src[row_index][element_index]
+        if first_num % 2 == 0 && second_num % 2 == 0
+        total += first_num + second_num
+        end
+        element_index += 1
+      end
+      row_index += 1
+    end
+  return total
 end
