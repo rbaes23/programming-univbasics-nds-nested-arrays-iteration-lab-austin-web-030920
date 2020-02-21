b033b87fd89b752f2d5dc1d3408441cc3970ec15
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-21 18:34:56 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -15,7 +15,7 @@ def join_ingredients(src)
           first_ingredient = src[row_index][element_index]
           element_index += 1
           second_ingredient = src[row_index][element_index]
-          new_array << "I love #{first_ingredient} and #{src[row_index][element_index]} on my pizza"
+          new_array << "I love #{first_ingredient} and #{second_ingredient} on my pizza"
         end
         row_index += 1
      end
