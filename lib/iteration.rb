0f4a1842b5334948a507e66ee969283441e93c8d
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-21 18:04:40 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,10 +9,10 @@ def join_ingredients(src)
   
   new_array = []
   row_index = 0
-    while row_index < spice_rack.count do
+    while row_index < src.count do
       element_index = 0
-        while element_index < spice_rack[row_index].count do
-          puts "I love #{}"
+        while element_index < src[row_index].count do
+          puts "I love "
             element_index += 1
         end
         row_index += 1
